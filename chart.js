ce70f07b7d1df978177e03bb27b091fdce71638b
Shahid Zaka <szaka@MacBook.local>
2019-01-30 11:47:01 -0800

    reading-documentation -- morning update

--- chart.js
+++ chart.js
@@ -1,1 +1,7 @@
 // Your code here!
+
+var chart = c3.generate({
+  data: {
+    url: '/data.js'
+  }
+});
--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 // use this data to generate your pie chart
+/*
 var data = [
   ['sleep', 8],
   ['eat', 3],
@@ -6,7 +7,7 @@ var data = [
   ['relax', 2],
   ['other', 4]
 ];
-
+*/
 /*
 sleep should be color #D81159
 eat should be color #8F2D56
@@ -14,3 +15,63 @@ code should be color #218380
 relax should be color #FBB13C
 other should be color #726DA8
 */
+
+// Default Chart
+var chart_default = c3.generate({
+    data: {
+        
+        columns: [
+            ['sleep', 8],
+            ['eat', 3],
+            ['code', 7],
+            ['relax', 2],
+            ['other', 4]
+        ],
+        type : 'pie',
+      
+        
+    },
+
+  // Default colors
+    color: {
+        pattern: ['blue', 'orange', 'green', 'red', 'purple']
+    },
+
+
+});
+
+
+
+/*
+
+// Different colors from the default colors 
+    color: {
+        pattern: ['#D81159', '#8F2D56', '#218380', '#FBB13C', '#726DA8']
+    },
+
+*/
+
+/*
+pie: {
+        label: {
+            format: function (value, ratio, id) {
+                return d3.format('v')(value * 100 / 100) + ' ' + 'hours';
+
+            }
+        }
+    
+
+        },
+
+*/
+
+
+
+
+
+
+
+
+
+
+
